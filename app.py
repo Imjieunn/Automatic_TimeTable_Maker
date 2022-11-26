757f4c7aca78be520e9ccb915b845bf5dcbe834a
Jieun Lim <jieun776121@gmail.com>
2022-11-26 19:13:29 +0900

    git ignore add

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/main')
+def index():
+    return render_template("main.html")
+    
+@app.route('/login.html')
+def index2():
+    return render_template("login.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
